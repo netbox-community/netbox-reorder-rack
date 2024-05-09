f942401e84d4889d244f6c7f5770560a54955a0b
Alexander Gittings <agitting96@gmail.com>
2024-05-09 09:29:51 +0000

    Add cookie cutter template templates

--- netbox_reorder_rack/tests/test_view.py
+++ netbox_reorder_rack/tests/test_view.py
@@ -1,6 +1,7 @@
+from core.models import ObjectType
+from dcim.models import Device
 from dcim.models import Rack
 from dcim.models import Site
-from django.contrib.contenttypes.models import ContentType
 from users.models import ObjectPermission
 from utilities.testing import TestCase
 
@@ -21,8 +22,7 @@ class ReorderRackTestCase(TestCase):
         obj_perm = ObjectPermission(name="Test permission", actions=["change", "view"])
         obj_perm.save()
         obj_perm.users.add(self.user)
-        ct = ContentType.objects.filter(model="device").first()
-        obj_perm.object_types.add(ct)
+        obj_perm.object_types.add(ObjectType.objects.get_for_model(Device))
         rack = Rack.objects.all().first()
 
         response = self.client.get(f"/dcim/racks/{rack.pk}/reorder/")
