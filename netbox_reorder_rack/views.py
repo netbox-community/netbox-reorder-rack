a5129ff870ddb0264b9e2ff57488d6c23d21a259
Alex Gittings <agitting96@gmail.com>
2024-09-27 09:59:24 +0000

    Fixes #18: Add images to reorder rack view

--- netbox_reorder_rack/views.py
+++ netbox_reorder_rack/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.generic import View
+from netbox.config import get_config
 from utilities.views import register_model_view
 
 
@@ -21,6 +22,23 @@ class ReorderView(LoginRequiredMixin, PermissionRequiredMixin, View):
 
     def get(self, request, pk):
         rack = get_object_or_404(Rack, pk=pk)
+        # Get the 'view' query parameter from the URL, default to 'images-and-labels' if not provided
+        selected_view = request.GET.get("view", "images-and-labels")
+
+        # Now you can use the `selected_view` variable to handle the specific logic
+        if selected_view == "images-and-labels":
+            # Logic for handling 'Images and Labels' view
+            images = True
+            labels = True
+        elif selected_view == "images-only":
+            # Logic for handling 'Images only' view
+            images = True
+            labels = False
+        elif selected_view == "labels-only":
+            # Logic for handling 'Labels only' view
+            images = False
+            labels = True
+
         non_racked = Device.objects.filter(
             rack=rack, position__isnull=True, parent_bay__isnull=True
         )
@@ -33,12 +51,19 @@ class ReorderView(LoginRequiredMixin, PermissionRequiredMixin, View):
                 exclude_list.append(device.id)
 
         non_racked_devices = non_racked.exclude(pk__in=exclude_list)
+        config = get_config()
+
+        base_url = f"{request.scheme}://{request.get_host().rstrip('/')}"
 
         return render(
             request,
             self.template_name,
             {
                 "object": rack,
+                "images": images,
+                "labels": labels,
+                "unit_width": config.RACK_ELEVATION_DEFAULT_UNIT_WIDTH,
+                "base_url": base_url,
                 "front_units": rack.get_rack_units(expand_devices=False, face="front"),
                 "rear_units": rack.get_rack_units(expand_devices=False, face="rear"),
                 "non_racked": non_racked_devices,
