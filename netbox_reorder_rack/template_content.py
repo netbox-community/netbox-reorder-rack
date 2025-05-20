2f5b471d3a9926080cfdbc7d191c2978bfa82d36
TsurumaruTsuyoshi <192761079+TsurumaruTsuyoshi@users.noreply.github.com>
2025-05-20 14:48:42 +0800

    Update template_content to models tuple

--- netbox_reorder_rack/template_content.py
+++ netbox_reorder_rack/template_content.py
@@ -2,7 +2,7 @@ from netbox.plugins import PluginTemplateExtension
 
 
 class ReorderButton(PluginTemplateExtension):
-    model = "dcim.rack"
+    models = ("dcim.rack", )
 
     def buttons(self):
         return self.render("netbox_reorder_rack/inc/rack_button.html")
