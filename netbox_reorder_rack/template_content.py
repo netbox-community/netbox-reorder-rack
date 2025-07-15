b9d7e7a355d72f93599c3bbb17cf90b45ab31804
Chris Russell <crussell@netboxlabs.com>
2025-07-15 19:43:34 +0100

    Update template_content.py

--- netbox_reorder_rack/template_content.py
+++ netbox_reorder_rack/template_content.py
@@ -2,7 +2,7 @@ from netbox.plugins import PluginTemplateExtension
 
 
 class ReorderButton(PluginTemplateExtension):
-    models = ["dcim.rack"]
+    models = ("dcim.rack",)
 
     def buttons(self):
         return self.render("netbox_reorder_rack/inc/rack_button.html")
